f9829166af82ac851352d6c7bec01a2ee3771fed
Saqib Rauf <saqibrauf@yahoo.com>
2019-04-16 21:26:18 +0500

    548452

--- QUOTES/settings.py
+++ QUOTES/settings.py
@@ -142,16 +142,3 @@ TAGGIT_CASE_INSENSITIVE = True
 
 SITE_ID = 1
 
-
-RANDOM_IMAGES = [
-    'images/bg1.jpeg',
-    'images/bg2.jpeg',
-    'images/bg3.jpeg',
-    'images/bg4.jpeg',
-    'images/bg5.jpeg',
-    'images/bg6.jpeg',
-    'images/bg7.jpeg',
-    'images/bg8.jpeg',
-    'images/bg9.jpeg',
-    'images/bg10.jpeg',
-]
--- quote/urls.py
+++ quote/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib.sitemaps import views as sm_views
 from .sitemap import AuthorSitemap, QuoteSitemap, TagSitemap
+from django.views.generic import TemplateView
 from . import views
 
 sitemaps = {
@@ -16,11 +17,12 @@ urlpatterns = [
 
 
     path('', views.index, name = 'index'),
-    path('<slug>/', views.content, name = 'content'), 
+    path('pages/<slug>/', views.content, name = 'content'), 
     path('quote/<slug>-<id>', views.quote, name = 'quote'),
     path('all-authors', views.all_authors, name = 'all_authors'),
     path('author/<slug>-quotes-<id>', views.author, name = 'author'),
     path('tags/<slug>-quotes', views.tag, name = 'tag'),
     path('search', views.search, name = 'search'),
     path('save-quote', views.save_quote, name = 'save_quote'),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt'), name="robot"),
 ]
