f2e65b500e13962af02bbd16098c577c32cfe5a0
Saqib Rauf <saqibrauf@yahoo.com>
2019-04-16 19:13:35 +0500

    548545445

--- quote/globals.py
+++ quote/globals.py
@@ -7,14 +7,14 @@ from django.db.models import Count
 def global_vars(request):
 	CONTENT = Content.objects.all()
 	AUTHORS = Author.objects.all().order_by('name')
-	#SIDEBAR_AUTHORS = Author.objects.annotate(num_quotes=Count('quotes')).order_by('-num_quotes')
-	#SIDEBAR_TOPICS = Tag.objects.annotate(num_quotes=Count('quote')).order_by('-num_quotes')
+	SIDEBAR_AUTHORS = Author.objects.annotate(num_quotes=Count('quotes')).order_by('-num_quotes')
+	SIDEBAR_TOPICS = Tag.objects.annotate(num_quotes=Count('quote')).order_by('-num_quotes')
 	#RANDOM_IMAGES = settings.RANDOM_IMAGES
 
 	return {
 		'CONTENT' : CONTENT,
 		'AUTHORS' : AUTHORS,
-		#'SIDEBAR_AUTHORS' : SIDEBAR_AUTHORS,
-		#'SIDEBAR_TOPICS' : SIDEBAR_TOPICS,
+		'SIDEBAR_AUTHORS' : SIDEBAR_AUTHORS,
+		'SIDEBAR_TOPICS' : SIDEBAR_TOPICS,
 		#'RANDOM_IMAGES' : RANDOM_IMAGES,
 	}
